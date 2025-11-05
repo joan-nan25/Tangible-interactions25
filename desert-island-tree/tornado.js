11598e9f60102e7b0a54f43d427c3147cd091409
Joa-Nan67 <99772804+joan-nan25@users.noreply.github.com>
2025-11-04 23:29:52 -0800

    .
    .

--- desert-island-tree/tornado.js
+++ desert-island-tree/tornado.js
@@ -1,4 +1,15 @@
-update(trees, sliderSpeed = 2) {
+class Tornado {
+  constructor(x, thunderSound, scenarioType) {
+    this.pos = createVector(x, 150);
+    this.vel = createVector(-2, 0); // make sure it's visible speed
+    this.scenarioType = scenarioType;
+    this.activated = false;
+    this.thunderSound = thunderSound;
+    this.flashAlpha = 0;
+    this.raindrops = [];
+  }
+
+update(trees, sliderSpeed = 4) {
   // horizontal movement based on slider
   this.pos.x -= sliderSpeed;
 
@@ -33,4 +44,47 @@ update(trees, sliderSpeed = 2) {
 
   stormColorFactor = lerp(stormColorFactor, 0, 0.05);
   this.flashAlpha *= 0.9;
+}
+
+
+  display() {
+    // cloud
+    noStroke();
+    fill(100);
+    ellipse(this.pos.x, this.pos.y, 140, 70);
+
+    // funnel
+    this.drawTornado();
+
+    // rain
+    stroke(150, 150, 255, 180);
+    strokeWeight(2);
+    for (let r of this.raindrops) {
+      line(r.x, r.y, r.x, r.y + 12);
+    }
+
+    // lightning flash overlay
+    if (this.flashAlpha > 5) {
+      noStroke();
+      fill(255, 255, 230, this.flashAlpha);
+      rect(0, 0, width, height);
+    }
+  }
+
+  drawTornado() {
+    push();
+    translate(this.pos.x, this.pos.y + 35);
+    noFill();
+    stroke(180, 180, 200, 180);
+    strokeWeight(3);
+    beginShape();
+    for (let y = 0; y < 150; y += 10) {
+      let w = map(y, 0, 150, 40, 5);
+      let x = sin(frameCount * 0.2 + y * 0.3) * 6;
+      vertex(x - w / 2, y);
+      vertex(x + w / 2, y);
+    }
+    endShape();
+    pop();
+  }
 }
